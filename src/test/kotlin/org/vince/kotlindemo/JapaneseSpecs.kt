6824fe9ec411ba1e8792523f1c0dd904f49b51e3
Vincent Marmin <vincent.marmin@davidson.fr>
2017-06-03 00:05:17 +0200

    feat(test): add japanese DSL test

---
+++ src/test/kotlin/org/vince/kotlindemo/JapaneseSpecs.kt
@@ -0,0 +1,31 @@
+package org.vince.kotlindemo
+
+import org.amshove.kluent.shouldEqualTo
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+
+object JapaneseSpec : Spek({
+
+    given("One person Yamamoto") {
+        val この人 = 人(名前 = "山本")
+
+        it("This person's name should be 'Yamamoto'") {
+            この人 の 名前 は "山本" です か
+            // thisPerson.name shouldEquals "Yamamoto"
+        }
+    }
+})
+
+val は = null
+val か = null
+
+infix fun Pair<String,String>.です(other: Nothing?) = this.first shouldEqualTo this.second
+
+infix fun String.は(other: String) = Pair(this, other)
+
+val 名前 = 人::名前
+
+data class 人(var 名前: String) {
+    infix fun の(f: (人) -> String) = f.invoke(this)
+}
